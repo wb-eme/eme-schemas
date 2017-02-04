2730e9771fc73b7abb7f724268fe7a70c207d5d3
Greg Brown <greg@12dnetworks.com>
2017-02-04 14:28:43 -0800

    removed TITLE_DESC/TITLE_ASC from SearchSort in the Users service in favor or more specific sort options.
    When searching for a user within elasticsearch the "title" field is likely tokenized in a such a way that
    attempting to sort by that field will produce what seems like invalid results.  For example, searching
    for 'Adam Zebra' would be sorted first in both scenarios (asc/desc) since elasticsearch will split the
    value and have "Adam" and "Zebra" as separate values when using a standard or english analyzer.
    
    Technically it's working as expected but it's non-obvious.  For clarity we'll have separate sort options
    for the different fields.   Of course a user with multiple names in first/last will again produce the
    same issue but it's much less likely to occur.  For example. "Chad Michael Murray" or "Edward James Almos"
    or "Joseph Gordon Levitt" may still sort in what seems like an invalid order.
    
    I probably spelled those actor names incorrectly, calm down, it's fine.  The point is, sorting isn't
    perfect when using full-text search unless you use a custom analyzer for the field use need sorting
    on.  If you the sort must be perfect, do that in a custom mapper and have the query use that field for sort.

--- src/php/Eme/Schemas/Users/Enum/SearchSort.php
+++ src/php/Eme/Schemas/Users/Enum/SearchSort.php
@@ -11,8 +11,10 @@ use Gdbots\Common\Enum;
  * @method static SearchSort CREATED_AT_ASC()
  * @method static SearchSort UPDATED_AT_DESC()
  * @method static SearchSort UPDATED_AT_ASC()
- * @method static SearchSort TITLE_DESC()
- * @method static SearchSort TITLE_ASC()
+ * @method static SearchSort FIRST_NAME_DESC()
+ * @method static SearchSort FIRST_NAME_ASC()
+ * @method static SearchSort LAST_NAME_DESC()
+ * @method static SearchSort LAST_NAME_ASC()
  */
 final class SearchSort extends Enum
 {
@@ -22,6 +24,8 @@ final class SearchSort extends Enum
     const CREATED_AT_ASC = 'created-at-asc';
     const UPDATED_AT_DESC = 'updated-at-desc';
     const UPDATED_AT_ASC = 'updated-at-asc';
-    const TITLE_DESC = 'title-desc';
-    const TITLE_ASC = 'title-asc';
+    const FIRST_NAME_DESC = 'first-name-desc';
+    const FIRST_NAME_ASC = 'first-name-asc';
+    const LAST_NAME_DESC = 'last-name-desc';
+    const LAST_NAME_ASC = 'last-name-asc';
 }
