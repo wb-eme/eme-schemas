3bf01016a8deae65041b71454376da755dd1a4cf
bhsiao55 <brian.hsiao@wbdevops.com>
2017-08-29 15:33:36 -0700

    add new field, form-published, publish-form schemas

--- build/js/src/eme/solicits/command/PublishSolicitV1.js
+++ build/js/src/eme/solicits/command/PublishSolicitV1.js
@@ -1,12 +1,11 @@
 // @link http://schemas.wbeme.com/json-schema/eme/solicits/command/publish-solicit/1-0-0.json#
 import EmeAccountsAccountRefV1Mixin from '@wbeme/schemas/eme/accounts/mixin/account-ref/AccountRefV1Mixin';
-import Fb from '@gdbots/pbj/FieldBuilder';
+import GdbotsFormsPublishFormV1Mixin from '@gdbots/schemas/gdbots/forms/mixin/publish-form/PublishFormV1Mixin';
 import GdbotsPbjxCommandV1Mixin from '@gdbots/schemas/gdbots/pbjx/mixin/command/CommandV1Mixin';
 import GdbotsPbjxCommandV1Trait from '@gdbots/schemas/gdbots/pbjx/mixin/command/CommandV1Trait';
 import Message from '@gdbots/pbj/Message';
 import MessageResolver from '@gdbots/pbj/MessageResolver';
 import Schema from '@gdbots/pbj/Schema';
-import T from '@gdbots/pbj/types';
 
 export default class PublishSolicitV1 extends Message {
   /**
@@ -16,14 +15,11 @@ export default class PublishSolicitV1 extends Message {
    */
   static defineSchema() {
     return new Schema('pbj:eme:solicits:command:publish-solicit:1-0-0', PublishSolicitV1,
-      [
-        Fb.create('solicit_id', T.IdentifierType.create())
-          .required()
-          .build(),
-      ],
+      [],
       [
         EmeAccountsAccountRefV1Mixin.create(),
         GdbotsPbjxCommandV1Mixin.create(),
+        GdbotsFormsPublishFormV1Mixin.create(),
       ],
     );
   }
