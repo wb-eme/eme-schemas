7927d20df71944891e79331a5fa61464b38c8619
bhsiao55 <brian.hsiao@wbdevops.com>
2017-08-30 15:33:38 -0700

    add ncr search nodes mixin to search solicit schema

--- build/js/src/eme/solicits/request/SearchSolicitsResponseV1.js
+++ build/js/src/eme/solicits/request/SearchSolicitsResponseV1.js
@@ -1,5 +1,6 @@
 // @link http://schemas.wbeme.com/json-schema/eme/solicits/request/search-solicits-response/1-0-0.json#
 import GdbotsFormsSearchFormsResponseV1Mixin from '@gdbots/schemas/gdbots/forms/mixin/search-forms-response/SearchFormsResponseV1Mixin';
+import GdbotsNcrSearchNodesResponseV1Mixin from '@gdbots/schemas/gdbots/ncr/mixin/search-nodes-response/SearchNodesResponseV1Mixin';
 import GdbotsPbjxResponseV1Mixin from '@gdbots/schemas/gdbots/pbjx/mixin/response/ResponseV1Mixin';
 import GdbotsPbjxResponseV1Trait from '@gdbots/schemas/gdbots/pbjx/mixin/response/ResponseV1Trait';
 import Message from '@gdbots/pbj/Message';
@@ -18,6 +19,7 @@ export default class SearchSolicitsResponseV1 extends Message {
       [
         GdbotsPbjxResponseV1Mixin.create(),
         GdbotsFormsSearchFormsResponseV1Mixin.create(),
+        GdbotsNcrSearchNodesResponseV1Mixin.create(),
       ],
     );
   }
--- build/php/src/Eme/Schemas/Solicits/Request/SearchSolicitsResponseV1.php
+++ build/php/src/Eme/Schemas/Solicits/Request/SearchSolicitsResponseV1.php
@@ -6,6 +6,8 @@ use Gdbots\Pbj\AbstractMessage;
 use Gdbots\Pbj\Schema;
 use Gdbots\Schemas\Forms\Mixin\SearchFormsResponse\SearchFormsResponseV1 as GdbotsFormsSearchFormsResponseV1;
 use Gdbots\Schemas\Forms\Mixin\SearchFormsResponse\SearchFormsResponseV1Mixin as GdbotsFormsSearchFormsResponseV1Mixin;
+use Gdbots\Schemas\Ncr\Mixin\SearchNodesResponse\SearchNodesResponseV1 as GdbotsNcrSearchNodesResponseV1;
+use Gdbots\Schemas\Ncr\Mixin\SearchNodesResponse\SearchNodesResponseV1Mixin as GdbotsNcrSearchNodesResponseV1Mixin;
 use Gdbots\Schemas\Pbjx\Mixin\Response\ResponseV1 as GdbotsPbjxResponseV1;
 use Gdbots\Schemas\Pbjx\Mixin\Response\ResponseV1Mixin as GdbotsPbjxResponseV1Mixin;
 use Gdbots\Schemas\Pbjx\Mixin\Response\ResponseV1Trait as GdbotsPbjxResponseV1Trait;
@@ -13,7 +15,8 @@ use Gdbots\Schemas\Pbjx\Mixin\Response\ResponseV1Trait as GdbotsPbjxResponseV1Tr
 final class SearchSolicitsResponseV1 extends AbstractMessage implements
     SearchSolicitsResponse,
     GdbotsPbjxResponseV1,
-    GdbotsFormsSearchFormsResponseV1
+    GdbotsFormsSearchFormsResponseV1,
+    GdbotsNcrSearchNodesResponseV1
 {
     use GdbotsPbjxResponseV1Trait;
 
@@ -27,6 +30,7 @@ final class SearchSolicitsResponseV1 extends AbstractMessage implements
             [
                 GdbotsPbjxResponseV1Mixin::create(),
                 GdbotsFormsSearchFormsResponseV1Mixin::create(),
+                GdbotsNcrSearchNodesResponseV1Mixin::create(),
             ]
         );
     }
