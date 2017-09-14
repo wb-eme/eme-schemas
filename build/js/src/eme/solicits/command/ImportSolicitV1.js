713e66306d0d04ce20c8e28a39ef3d498a779343
bhsiao55 <brian.hsiao@wbdevops.com>
2017-09-13 18:16:23 -0700

    add category and form mixin to import schema

--- build/js/src/eme/solicits/command/ImportSolicitV1.js
+++ build/js/src/eme/solicits/command/ImportSolicitV1.js
@@ -1,8 +1,10 @@
 // @link http://schemas.wbeme.com/json-schema/eme/solicits/command/import-solicit/1-0-0.json#
 import EmeAccountsAccountRefV1Mixin from '@wbeme/schemas/eme/accounts/mixin/account-ref/AccountRefV1Mixin';
 import Fb from '@gdbots/pbj/FieldBuilder';
-import Format from '@gdbots/pbj/enums/Format';
 import GdbotsCommonTaggableV1Mixin from '@gdbots/schemas/gdbots/common/mixin/taggable/TaggableV1Mixin';
+import GdbotsFormsFormV1Mixin from '@gdbots/schemas/gdbots/forms/mixin/form/FormV1Mixin';
+import GdbotsNcrNodeV1Mixin from '@gdbots/schemas/gdbots/ncr/mixin/node/NodeV1Mixin';
+import GdbotsNcrNodeV1Trait from '@gdbots/schemas/gdbots/ncr/mixin/node/NodeV1Trait';
 import GdbotsPbjxCommandV1Mixin from '@gdbots/schemas/gdbots/pbjx/mixin/command/CommandV1Mixin';
 import GdbotsPbjxCommandV1Trait from '@gdbots/schemas/gdbots/pbjx/mixin/command/CommandV1Trait';
 import Message from '@gdbots/pbj/Message';
@@ -25,15 +27,7 @@ export default class ImportSolicitV1 extends Message {
           .build(),
         Fb.create('title', T.StringType.create())
           .build(),
-        /*
-         * A short description (a few sentences) about this solicit. This field should
-         * not have html as it is used in metadata.
-         */
-        Fb.create('description', T.TextType.create())
-          .build(),
-        Fb.create('hashtags', T.StringType.create())
-          .asASet()
-          .format(Format.HASHTAG)
+        Fb.create('category', T.StringType.create())
           .build(),
         Fb.create('story_enabled', T.BooleanType.create())
           .withDefault(true)
@@ -42,20 +36,20 @@ export default class ImportSolicitV1 extends Message {
           .build(),
         Fb.create('is_active', T.BooleanType.create())
           .build(),
-        Fb.create('created_at', T.MicrotimeType.create())
-          .useTypeDefault(false)
-          .build(),
       ],
       [
         EmeAccountsAccountRefV1Mixin.create(),
         GdbotsPbjxCommandV1Mixin.create(),
         GdbotsCommonTaggableV1Mixin.create(),
+        GdbotsNcrNodeV1Mixin.create(),
+        GdbotsFormsFormV1Mixin.create(),
       ],
     );
   }
 }
 
 GdbotsPbjxCommandV1Trait(ImportSolicitV1);
+GdbotsNcrNodeV1Trait(ImportSolicitV1);
 MessageResolver.register('eme:solicits:command:import-solicit', ImportSolicitV1);
 Object.freeze(ImportSolicitV1);
 Object.freeze(ImportSolicitV1.prototype);
--- build/php/src/Eme/Schemas/Solicits/Command/ImportSolicitV1.php
+++ build/php/src/Eme/Schemas/Solicits/Command/ImportSolicitV1.php
@@ -6,12 +6,16 @@ use Eme\Schemas\Accounts\Mixin\AccountRef\AccountRefV1 as EmeAccountsAccountRefV
 use Eme\Schemas\Accounts\Mixin\AccountRef\AccountRefV1Mixin as EmeAccountsAccountRefV1Mixin;
 use Eme\Schemas\Solicits\SolicitId;
 use Gdbots\Pbj\AbstractMessage;
-use Gdbots\Pbj\Enum\Format;
 use Gdbots\Pbj\FieldBuilder as Fb;
 use Gdbots\Pbj\Schema;
 use Gdbots\Pbj\Type as T;
 use Gdbots\Schemas\Common\Mixin\Taggable\TaggableV1 as GdbotsCommonTaggableV1;
 use Gdbots\Schemas\Common\Mixin\Taggable\TaggableV1Mixin as GdbotsCommonTaggableV1Mixin;
+use Gdbots\Schemas\Forms\Mixin\Form\FormV1 as GdbotsFormsFormV1;
+use Gdbots\Schemas\Forms\Mixin\Form\FormV1Mixin as GdbotsFormsFormV1Mixin;
+use Gdbots\Schemas\Ncr\Mixin\Node\NodeV1 as GdbotsNcrNodeV1;
+use Gdbots\Schemas\Ncr\Mixin\Node\NodeV1Mixin as GdbotsNcrNodeV1Mixin;
+use Gdbots\Schemas\Ncr\Mixin\Node\NodeV1Trait as GdbotsNcrNodeV1Trait;
 use Gdbots\Schemas\Pbjx\Mixin\Command\CommandV1 as GdbotsPbjxCommandV1;
 use Gdbots\Schemas\Pbjx\Mixin\Command\CommandV1Mixin as GdbotsPbjxCommandV1Mixin;
 use Gdbots\Schemas\Pbjx\Mixin\Command\CommandV1Trait as GdbotsPbjxCommandV1Trait;
@@ -20,9 +24,12 @@ final class ImportSolicitV1 extends AbstractMessage implements
     ImportSolicit,
     EmeAccountsAccountRefV1,
     GdbotsPbjxCommandV1,
-    GdbotsCommonTaggableV1
+    GdbotsCommonTaggableV1,
+    GdbotsNcrNodeV1,
+    GdbotsFormsFormV1
 {
     use GdbotsPbjxCommandV1Trait;
+    use GdbotsNcrNodeV1Trait;
 
     /**
      * @return Schema
@@ -36,15 +43,7 @@ final class ImportSolicitV1 extends AbstractMessage implements
                     ->build(),
                 Fb::create('title', T\StringType::create())
                     ->build(),
-                /*
-                 * A short description (a few sentences) about this solicit. This field should
-                 * not have html as it is used in metadata.
-                 */
-                Fb::create('description', T\TextType::create())
-                    ->build(),
-                Fb::create('hashtags', T\StringType::create())
-                    ->asASet()
-                    ->format(Format::HASHTAG())
+                Fb::create('category', T\StringType::create())
                     ->build(),
                 Fb::create('story_enabled', T\BooleanType::create())
                     ->withDefault(true)
@@ -53,14 +52,13 @@ final class ImportSolicitV1 extends AbstractMessage implements
                     ->build(),
                 Fb::create('is_active', T\BooleanType::create())
                     ->build(),
-                Fb::create('created_at', T\MicrotimeType::create())
-                    ->useTypeDefault(false)
-                    ->build(),
             ],
             [
                 EmeAccountsAccountRefV1Mixin::create(),
                 GdbotsPbjxCommandV1Mixin::create(),
                 GdbotsCommonTaggableV1Mixin::create(),
+                GdbotsNcrNodeV1Mixin::create(),
+                GdbotsFormsFormV1Mixin::create(),
             ]
         );
     }
