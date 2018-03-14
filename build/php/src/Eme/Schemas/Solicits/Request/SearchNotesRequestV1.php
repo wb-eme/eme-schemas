79e8652357fa0849d50fef741951d528a707a4a2
albertgcsula <Alberto.Gutierrez@telepixtv.com>
2018-03-14 14:49:36 -0700

    re #7 update gdbots schemas, remove fields from search notes

--- build/php/src/Eme/Schemas/Solicits/Request/SearchNotesRequestV1.php
+++ build/php/src/Eme/Schemas/Solicits/Request/SearchNotesRequestV1.php
@@ -5,11 +5,7 @@ namespace Eme\Schemas\Solicits\Request;
 use Eme\Schemas\Accounts\Mixin\AccountRef\AccountRefV1 as EmeAccountsAccountRefV1;
 use Eme\Schemas\Accounts\Mixin\AccountRef\AccountRefV1Mixin as EmeAccountsAccountRefV1Mixin;
 use Gdbots\Pbj\AbstractMessage;
-use Gdbots\Pbj\FieldBuilder as Fb;
 use Gdbots\Pbj\Schema;
-use Gdbots\Pbj\Type as T;
-use Gdbots\Schemas\Analytics\Mixin\TrackedMessage\TrackedMessageV1 as GdbotsAnalyticsTrackedMessageV1;
-use Gdbots\Schemas\Analytics\Mixin\TrackedMessage\TrackedMessageV1Mixin as GdbotsAnalyticsTrackedMessageV1Mixin;
 use Gdbots\Schemas\Pbjx\Mixin\Request\RequestV1 as GdbotsPbjxRequestV1;
 use Gdbots\Schemas\Pbjx\Mixin\Request\RequestV1Mixin as GdbotsPbjxRequestV1Mixin;
 use Gdbots\Schemas\Pbjx\Mixin\Request\RequestV1Trait as GdbotsPbjxRequestV1Trait;
@@ -20,8 +16,7 @@ final class SearchNotesRequestV1 extends AbstractMessage implements
     SearchNotesRequest,
     EmeAccountsAccountRefV1,
     GdbotsPbjxRequestV1,
-    GdbotsPbjxSearchEventsRequestV1,
-    GdbotsAnalyticsTrackedMessageV1
+    GdbotsPbjxSearchEventsRequestV1
 {
     use GdbotsPbjxRequestV1Trait;
 
@@ -31,18 +26,11 @@ final class SearchNotesRequestV1 extends AbstractMessage implements
     protected static function defineSchema()
     {
         return new Schema('pbj:eme:solicits:request:search-notes-request:1-0-0', __CLASS__,
-            [
-                Fb::create('submission_id', T\TimeUuidType::create())
-                    ->required()
-                    ->build(),
-                Fb::create('note', T\TextType::create())
-                    ->build(),
-            ],
+            [],
             [
                 EmeAccountsAccountRefV1Mixin::create(),
                 GdbotsPbjxRequestV1Mixin::create(),
                 GdbotsPbjxSearchEventsRequestV1Mixin::create(),
-                GdbotsAnalyticsTrackedMessageV1Mixin::create(),
             ]
         );
     }
